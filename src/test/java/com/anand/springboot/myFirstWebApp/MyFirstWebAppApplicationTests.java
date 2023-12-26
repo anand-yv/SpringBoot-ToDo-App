ac348ef71542d985481b21a886a3da1c08340b78
anand-yv <anand93056ad@gmail.com>
2023-12-26 10:57:16 +0530

    Initial commit

---
+++ src/test/java/com/anand/springboot/myFirstWebApp/MyFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.springboot.myFirstWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
